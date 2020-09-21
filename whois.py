8b17d9530ea014463869eb64412d8c72ff3f07d6
Evgeniy Nikitin <alterlikon@gmail.com>
2020-09-21 12:58:35 +0700

    Add more modules

--- whois.py
+++ whois.py
@@ -1,137 +1,126 @@
-"""Get Telegram Profile Picture and other information
-Syntax: .whois @username"""
+# Major change by @Fl1yd
+#
+# Channel: @ftgmodulesbyfl1yd
+#============================
 
-import html
+import os
+from .. import loader, utils
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
-from telethon.utils import get_input_location
-from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("whois ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    replied_user, error_i_a = await get_full_user(event)
-    if replied_user is None:
-        await event.edit(str(error_i_a))
-        return False
-    replied_user_profile_photos = await borg(GetUserPhotosRequest(
-        user_id=replied_user.user.id,
-        offset=42,
-        max_id=0,
-        limit=80
-    ))
-    replied_user_profile_photos_count = "NaN"
+def register(cb):
+    cb(WhoIsMod())
+
+class WhoIsMod(loader.Module):
+    """Получает информацию о пользователе в Телеграме (включая вас!)."""
+    strings = {'name': 'WhoIs'}
+
+    async def whoiscmd(self, whos):
+        """Используй .whois <@ или реплай>; ничего"""
+        await whos.edit("<b>Получаю информацию о пользователе...</b>")
+        replied_user = await get_user(whos)
+
+        try:
+            photo, caption = await fetch_info(replied_user, whos)
+        except AttributeError:
+            whos.edit("<b>Не могу найти информацию об этом пользователе.</b>")
+            return
+
+        message_id_to_reply = whos.reply_to_msg_id
+        if not message_id_to_reply:
+            message_id_to_reply = None
+
+        try:
+            await whos.client.send_file(whos.chat_id, photo, caption=caption,
+                                        link_preview=False, force_document=False,
+                                        reply_to=message_id_to_reply, parse_mode="html")
+            if not photo.startswith("http"):
+                os.remove(photo)
+            await whos.delete()
+        except TypeError:
+            await whos.edit(caption, parse_mode="html")
+
+
+async def get_user(event):
+    """Получение информации о пользователе с реплая или аргумента."""
+    if event.reply_to_msg_id and not utils.get_args_raw(event):
+        previous_message = await event.get_reply_message()
+        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
+    else:
+        user = utils.get_args_raw(event)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.entities is not None:
+            probable_user_mention_entity = event.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
+        except:
+            self_user = await event.client.get_me()
+            user = self_user.id
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
+            return replied_user
+    return replied_user
+
+
+async def fetch_info(replied_user, event):
+    """Подробная информация о пользователе."""
+    replied_user_profile_photos = await event.client(GetUserPhotosRequest(user_id=replied_user.user.id,
+                                                                          offset=42, max_id=0, limit=80))
+    replied_user_profile_photos_count = "Пользователю нужна помощь с загрузкой аватарки."
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
     except AttributeError as e:
         pass
     user_id = replied_user.user.id
-    # some people have weird HTML in their names
-    first_name = html.escape(replied_user.user.first_name)
-    # https://stackoverflow.com/a/5072031/4723940
-    # some Deleted Accounts do not have first_name
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their names
-        first_name = first_name.replace("\u2060", "")
-    # inspired by https://telegram.dog/afsaI181
+    first_name = replied_user.user.first_name
+    last_name = replied_user.user.last_name
+    common_chat = replied_user.common_chats_count
+    username = replied_user.user.username
     user_bio = replied_user.about
-    if user_bio is not None:
-        user_bio = html.escape(replied_user.about)
-    common_chats = replied_user.common_chats_count
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except Exception as e:
-        dc_id = "Need a Profile Picture to check **this**"
-        location = str(e)
-    caption = """ID: <code>{}</code>
-Name: <a href='tg://user?id={}'>{}</a>
-Bio: {}
-DC ID: {}
-Number of PPs: {}
-Restricted: {}
-Verified: {}
-Bot: {}
-Groups in Common: {}
-""".format(
-        user_id,
-        user_id,
-        first_name,
-        user_bio,
-        dc_id,
-        replied_user_profile_photos_count,
-        replied_user.user.restricted,
-        replied_user.user.verified,
-        replied_user.user.bot,
-        common_chats
-    )
-    message_id_to_reply = event.message.reply_to_msg_id
-    if not message_id_to_reply:
-        message_id_to_reply = event.message.id
-    await borg.send_message(
-        event.chat_id,
-        caption,
-        reply_to=message_id_to_reply,
-        parse_mode="HTML",
-        file=replied_user.profile_photo,
-        force_document=False,
-        silent=True
-    )
-    await event.delete()
-
-
-async def get_full_user(event):
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.forward:
-            replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.forward.from_id or previous_message.forward.channel_id
-                )
-            )
-            return replied_user, None
-        else:
-            replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.from_id
-                )
-            )
-            return replied_user, None
+    is_bot = replied_user.user.bot
+    if is_bot == False:
+        is_bot = "Нет"
     else:
-        input_str = None
-        try:
-            input_str = event.pattern_match.group(1)
-        except IndexError as e:
-            return None, e
-        if event.message.entities is not None:
-            mention_entity = event.message.entities
-            probable_user_mention_entity = mention_entity[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            else:
-                try:
-                    user_object = await event.client.get_entity(input_str)
-                    user_id = user_object.id
-                    replied_user = await event.client(GetFullUserRequest(user_id))
-                    return replied_user, None
-                except Exception as e:
-                    return None, e
-        elif event.is_private:
-            try:
-                user_id = event.chat_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
-        else:
-            try:
-                user_object = await event.client.get_entity(int(input_str))
-                user_id = user_object.id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
+        is_bot = "Да"
+    restricted = replied_user.user.restricted
+    if restricted == False:
+        restricted = "Нет"
+    else:
+        restricted = "Да"
+    verified = replied_user.user.verified
+    if verified == False:
+        verified = "Нет"
+    else:
+        verified = "Да"
+    photo = await event.client.download_profile_photo(user_id, str(user_id) + ".jpg", download_big=True)
+    first_name = first_name.replace("\u2060", "") if first_name else ("Пользователь не указал имя.")
+    last_name = last_name.replace("\u2060", "") if last_name else ("Пользователь не указал фамилии.")
+    username = "@{}".format(username) if username else ("У пользователя нету юзернейма.")
+    user_bio = "У пользователя нету информации о себе." if not user_bio else user_bio
+
+    caption = "<b>ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ:</b>\n\n"
+    caption += f"<b>Имя:</b> {first_name}\n"
+    caption += f"<b>Фамилия:</b> {last_name}\n"
+    caption += f"<b>Юзернейм:</b> {username}\n"
+    caption += f"<b>ID:</b> <code>{user_id}</code>\n"
+    caption += f"<b>Бот:</b> {is_bot}\n"
+    caption += f"<b>Ограничен:</b> {restricted}\n"
+    caption += f"<b>Верифицирован:</b> {verified}\n\n"
+    caption += f"<b>О себе:</b> \n<code>{user_bio}</code>\n\n"
+    caption += f"<b>Кол-во аватарок в профиле:</b> {replied_user_profile_photos_count}\n"
+    caption += f"<b>Общие чаты:</b> {common_chat}\n"
+    caption += f"<b>Пермалинк:</b> "
+    caption += f"<a href=\"tg://user?id={user_id}\">клик</a>"
+
+    return photo, caption
